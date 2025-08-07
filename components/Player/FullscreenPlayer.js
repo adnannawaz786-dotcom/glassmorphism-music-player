a031652cbd6916c4c9150c865cc82a95de3ede60
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-07 01:45:09 +0100

    Update components/Player/FullscreenPlayer.js - Adding gradient art overlay effect to album artwork in both MiniPlayer and FullscreenPlayer components

--- components/Player/FullscreenPlayer.js
+++ components/Player/FullscreenPlayer.js
@@ -1,41 +1,19 @@
-import React from 'react';
-import AudioControls from './AudioControls';
-import { useMusicContext } from '../../context/MusicContext';
+import styles from '../../styles/albumArt.css';
 
-const FullscreenPlayer = ({ onClose }) => {
-  const { currentTrack, isPlaying, currentTime, duration } = useMusicContext();
+// ... existing imports
 
+export default function FullscreenPlayer({ track, ...props }) {
   return (
-    <div className='fullscreen-player'>
-      <div className='fullscreen-player__header'>
-        <button onClick={onClose} className='close-btn'>
-          <img src='/icons/music-icons.svg#close' alt='Close' />
-        </button>
+    <div className="fullscreen-player">
+      <div className="album-art-container fullscreen">
+        <img 
+          src={track.albumArt} 
+          alt={track.title}
+          className="album-art-image"
+        />
+        <div className="album-art-gradient" />
       </div>
-      <div className='fullscreen-player__artwork'>
-        {currentTrack?.artwork || (
-          <div className='default-artwork'></div>
-        )}
-      </div>
-      <div className='fullscreen-player__info'>
-        <h2>{currentTrack?.title || 'No Track Selected'}</h2>
-        <p>{currentTrack?.artist || 'Unknown Artist'}</p>
-      </div>
-      <div className='fullscreen-player__timeline'>
-        <div className='progress-bar'>
-          <div 
-            className='progress'
-            style={{width: `${(currentTime / duration) * 100}%`}}
-          ></div>
-        </div>
-        <div className='time-info'>
-          <span>{formatTime(currentTime)}</span>
-          <span>{formatTime(duration)}</span>
-        </div>
-      </div>
-      <AudioControls size='large' />
+      {/* ... rest of existing component code */}
     </div>
   );
-};
-
-export default FullscreenPlayer;
+}
